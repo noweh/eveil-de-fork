1fb48b8ab95259c29e5adf74aed36efd3f05f417
SCHMITT Julien <j.schmitt@mazarine.com>
2025-05-23 14:37:08 +0200

    Manual deploy with corrected templates

--- website/dev-server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Nettoyage du dossier public...
-if exist public\ (
-    rmdir /s /q public
-)
-echo Lancement du serveur Hugo...
-hugo server -D 
--- website/layouts/footer.md
+++
@@ -1,1 +0,0 @@
-© 2025 - L'éveil de \\/Fork - Cyberfantasy par Julien SCHMITT
